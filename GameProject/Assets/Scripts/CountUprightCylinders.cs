71965c05fc0cf18de4b903d4069620c89c9582ee
kevinxiao712 <66409722+kevinxiao712@users.noreply.github.com>
2023-07-08 17:49:45 +0800

    Level1

--- GameProject/Assets/Scripts/CountUprightCylinders.cs
+++ GameProject/Assets/Scripts/CountUprightCylinders.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 using Cinemachine;
 public class CountUprightCylinders : MonoBehaviour
 {
-    private bool countdownStarted = false;
-    private float countdownTimer = 5f;
+    private bool countdownStarted=false;
+    private float countdownTimer=5f;
     public CinemachineVirtualCamera virtualCamera;
     void Update()
     {
         if (countdownStarted)
         {
-            countdownTimer -= Time.deltaTime;
-            if (countdownTimer <= 0)
+            countdownTimer-=Time.deltaTime;
+            if (countdownTimer<=0)
             {
                 CheckPins();
-                countdownStarted = false;
+                countdownStarted=false;
             }
         }
     }
@@ -23,29 +23,30 @@ public class CountUprightCylinders : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Ball")) 
         {
-            countdownStarted = true;
-            countdownTimer = 5f;
-            virtualCamera.Follow = null;
+            countdownStarted=true;
+            countdownTimer=5f;
+            virtualCamera.Follow=null;
+            virtualCamera.LookAt=null;
         }
     }
 
     void CheckPins()
     {
-        GameObject[] pins = GameObject.FindGameObjectsWithTag("Pins");
+        GameObject[] pins=GameObject.FindGameObjectsWithTag("Pins");
 
-        int uprightCount = 0;
+        int uprightCount=0;
 
         foreach (GameObject pin in pins)
         {
-            IsUpright uprightCheck = pin.GetComponent<IsUpright>();
+            IsUpright uprightCheck=pin.GetComponent<IsUpright>();
 
-            if (uprightCheck != null && uprightCheck.CheckUpright())
+            if (uprightCheck!=null && uprightCheck.CheckUpright())
             {
                 uprightCount++;
             }
         }
 
-        if (uprightCount == 0)
+        if (uprightCount==0)
         {
             Debug.Log("Down");
         }
