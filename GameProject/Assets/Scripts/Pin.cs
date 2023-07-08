099275a4c144714a9c5d21247afc09c0dc1c16ba
water0300 <yuen.nathan@yahoo.com>
2023-07-08 19:31:09 +0800

    object resetter

--- GameProject/Assets/Scripts/Pin.cs
+++ GameProject/Assets/Scripts/Pin.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IsUpright : MonoBehaviour
+public class Pin : MonoBehaviour
 {
     public float uprightThreshold=1f;
 
