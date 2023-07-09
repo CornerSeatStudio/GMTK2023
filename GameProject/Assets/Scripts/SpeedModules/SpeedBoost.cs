30ce7cb9e7b01e3281cec3beb282a303122fd772
kevinxiao712 <66409722+kevinxiao712@users.noreply.github.com>
2023-07-09 11:44:59 +0800

    spedforce

--- GameProject/Assets/Scripts/SpeedModules/SpeedBoost.cs
+++ GameProject/Assets/Scripts/SpeedModules/SpeedBoost.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class SpeedBoost : SpeedModifyZone
 {
-
+    public float spinForce = 1f;
     public float speedForce;
 
     protected override void DoTheMagic(BallMovement ball)
     {
+        Vector3 torqueDirection = transform.right;
         Vector3 currDir =  ball.rb.velocity.normalized;
-        ball.rb.AddForce(Vector3.up * speedForce, ForceMode.Impulse); //CHANGE THIS
+        ball.rb.AddForce(Vector3.forward * speedForce, ForceMode.Impulse);
+        ball.rb.AddTorque(torqueDirection * spinForce, ForceMode.Impulse);
     }
 }
