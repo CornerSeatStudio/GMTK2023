3696880c358311f4d1bf02ee73aa69e9d6874049
kevinxiao712 <66409722+kevinxiao712@users.noreply.github.com>
2023-07-09 11:07:57 +0800

    Update ObjectResetter.cs

--- GameProject/Assets/Scripts/ObjectResetter.cs
+++ GameProject/Assets/Scripts/ObjectResetter.cs
@@ -73,7 +73,7 @@ public class ObjectResetter : MonoBehaviour
 
         ball.transform.position = initBallTransform.position;
         ball.transform.rotation = initBallTransform.rotation;
-        ball.GetComponent<StartMove>().launched = false;
+        ball.GetComponent<BallMovement>().launched = false;
 
         foreach(var (go, t) in resetTransformData)
         {
