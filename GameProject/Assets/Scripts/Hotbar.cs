634c1b7c274a05176d2c26273bd370f6b54ebbe3
water0300 <yuen.nathan@yahoo.com>
2023-07-09 13:28:05 +0800

    menus

--- GameProject/Assets/Scripts/Hotbar.cs
+++ GameProject/Assets/Scripts/Hotbar.cs
@@ -6,14 +6,17 @@ using UnityEngine.EventSystems;
 
 public class Hotbar : MonoBehaviour {
     // Start is called before the first frame update
+    private RectTransform rTransform;
     public LevelEditor levelEditor;
     public HotbarSlot[] slots;
     public Sprite selectedSlot;
     public Sprite unselectedSlot;
-
+    public float swiftyOffset = 60f;
+    public float swiftySpeed = 5f;
 
     private void Awake()
     {
+        rTransform = GetComponent<RectTransform>(); 
         levelEditor = FindObjectOfType<LevelEditor>();
         slots = GetComponentsInChildren<HotbarSlot>();
     }
@@ -25,6 +28,23 @@ public class Hotbar : MonoBehaviour {
         //{
         //    slot.SetInactiveFrame();
         //}
+
+        StartCoroutine(CoolMove());
+    }
+
+    IEnumerator CoolMove()
+    {
+        float t = 0;
+        Vector3 initPos = rTransform.localPosition;
+        rTransform.localPosition -= Vector3.right * swiftyOffset;
+        while(t < 1f)
+        {
+            rTransform.localPosition = Vector3.Lerp(rTransform.localPosition, initPos, Time.deltaTime * swiftySpeed);
+            t += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        rTransform.localPosition = initPos;
     }
 
     // Update is called once per frame
