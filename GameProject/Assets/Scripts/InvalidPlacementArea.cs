85ff883bc23885e0e1f6aadc9d7257eff53ea71a
water0300 <yuen.nathan@yahoo.com>
2023-07-09 15:46:36 +0800

    invalid no pin zone

--- GameProject/Assets/Scripts/InvalidPlacementArea.cs
+++ GameProject/Assets/Scripts/InvalidPlacementArea.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class InvalidPlacementArea : MonoBehaviour
 {
     private BoxCollider m_Collider;
-
+    public bool NoPinsOnly;
     private void Awake()
     {
         m_Collider = GetComponent<BoxCollider>();
